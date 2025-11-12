c1f47a00282d42dc49a8da6f0730d73940203855
Tathagat155 <tathagatkumar70618@gmail.com>
2025-11-12 17:01:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function makeid(l) {
   // write your code here
+	let str="ABCDEFGHIJKLMNOPQR12345678";
+	let randomString="";
+	let index;
+	for(let i=0;i<l;i++){
+		index=Math.floor(Math.random()*str.length);
+		randomString+=str[index];
+	}
+	return randomString;
 }
-
-// Do not change the code below.
 const l = prompt("Enter a number.");
 alert(makeid(l));
+
+
+// Do not change the code below.
